6ee7848b1b7127bd89171df59088c10763163154
Amanda <amanda.hamynen@gmail.com>
2021-04-20 19:33:59 +0300

    Lisätty testejä

--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -7,6 +7,8 @@ class TestUserRepository(unittest.TestCase):
     def setUp(self):
         user_repository.delete_all()
         self.user_amanda = User('Amanda', 'a', 0)
+        self.user_toinen = User('Toinen', 't', 0)
+        self.user_kolmas = User('Kolmas', 'k', 0)
 
     def test_creation(self):
         user_repository.create(self.user_amanda)
@@ -14,3 +16,31 @@ class TestUserRepository(unittest.TestCase):
 
         self.assertEqual(all_users[0].username, self.user_amanda.username)
         self.assertEqual(len(all_users), 1)
+
+    def test_find_by_username(self):
+        user_repository.create(self.user_toinen)
+        user = user_repository.find_by_username(self.user_toinen.username)
+        self.assertEqual(user.username, self.user_toinen.username)
+
+    def test_find_all(self):
+        user_repository.create(self.user_amanda)
+        user_repository.create(self.user_toinen)
+        user_repository.create(self.user_kolmas)
+        users = user_repository.find_all()
+
+        self.assertEqual(users[0].username, self.user_amanda.username)
+        self.assertEqual(users[1].username, self.user_toinen.username)
+        self.assertEqual(users[2].username, self.user_kolmas.username)
+
+        self.assertEqual(len(users), 3)
+
+    def test_delete_all(self):
+        user_repository.create(self.user_amanda)
+        user_repository.create(self.user_toinen)
+        
+        users = user_repository.find_all()
+        self.assertEqual(len(users), 2)
+
+        user_repository.delete_all()
+        users = user_repository.find_all()
+        self.assertEqual(len(users), 0)
